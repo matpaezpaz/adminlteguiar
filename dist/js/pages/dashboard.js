f40409791442b295a40185e21cffa9535c5a8044
Matias <matpaezpaz@gmail.com>
2017-12-17 20:20:24 -0300

    Agrega el resto de los controladores y login

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -183,7 +183,7 @@ $(function () {
     resize   : true,
     colors   : ['#3c8dbc', '#f56954', '#00a65a'],
     data     : [
-      { label: 'Download Sales', value: 12 },
+      { label: 'ASD', value: 65 },
       { label: 'In-Store Sales', value: 30 },
       { label: 'Mail-Order Sales', value: 20 }
     ],
